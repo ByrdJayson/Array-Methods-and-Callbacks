cfecbdf29e378e91aa76e95ef0d72b53c1434e53
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-09-14 21:02:03 -0400

    Working Task 6

--- index.js
+++ index.js
@@ -108,8 +108,8 @@ Use the higher order function getAverageGoals to do the following:
  Example of invocation: getAverageGoals(getFinals(fifaData));
 */
 
-function getAverageGoals(/* code here */) {
-   /* code here */
+function getAverageGoals(arr, getFinals) {
+    
 }
 
 
